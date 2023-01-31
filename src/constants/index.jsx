f95356b685164ea97e4d138786f2fac15865a082
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-01 01:19:07 +0800

    Navbar links
    Created constants for the navbar.
    Will be use for rendering the navigator links

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -3,3 +3,27 @@
  * This ensures that easier rendering for 
  * components and avoids too much lines of code
 */
+
+// Navigation Bar links
+const navLinks = [
+    {
+        id: 1,
+        title: 'home',
+    },
+    {
+        id: 2,
+        title: 'about',
+    },
+    {
+        id: 3,
+        title: 'portfolio',
+    },
+    {
+        id: 4,
+        title: 'skills',
+    },
+    {
+        id: 5,
+        title: 'contact',
+    },
+]
