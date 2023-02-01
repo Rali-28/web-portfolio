c0e0cc2d8ddf7264f15b7780a126fcd2e1887a1c
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-02 00:11:09 +0800

    Hero.jsx comments
    Added comments for easier reading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,21 @@ import { BiRightArrowCircle } from 'react-icons/bi'
 
 const Hero = () => {
   return (
-    <div name="home" className="w-full h-screen text-white bg-gradient-to-b from-black to-gray-800">
-      <div className="flex flex-col items-center justify-center h-full max-w-screen-lg px-3 mx-auto text-center md:text-left md:flex-row">
+    // Main div of the hero section
+    <div name="home" id="home" className="w-full h-screen text-white bg-gradient-to-b from-black to-gray-800">
 
-        <div className="flex flex-col justify-center h-full">
+      {/* 
+          Div for handling two sections of the webpage, the Introduction and profile picture
+          Shows the two section in a column on large screens and a row on small screens
+      */}
+      <div className="flex flex-col items-center justify-center h-full max-w-screen-lg px-3 mx-auto md:flex-row">
+
+        {/* 
+            Div for the introduction and the portfolio button
+            Texts and button are alinged on the left side for large screens
+            Aligned on the center for small screens
+        */}
+        <div className="flex flex-col justify-center h-full text-center md:text-left">
           <h2 className="text-4xl font-bold sm:text-6xl">
             I am a Junior Web Developer
           </h2>
@@ -19,17 +30,24 @@ const Hero = () => {
           </p>
 
           <div>
-            <div>
-              <button className="flex items-center px-6 py-3 my-2 rounded-md cursor-pointer group w-fit bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] mx-auto md:mx-0">
-                Portfolio 
-                  <span className="px-2 duration-300 group-hover:rotate-90"><BiRightArrowCircle size={30} /></span>
-              </button>
-            </div>
+            {/* The portfolio button that have acolor of cyan to blue
+                aligned as a column to make right arrow appear besides "Portfolio"
+            */}
+            <button className="flex items-center px-6 py-3 my-2 rounded-md cursor-pointer group w-fit bg-gradient-to-r from-cyan-500 to-blue-500 text-[25px] mx-auto md:mx-0">
+              Portfolio 
+              {/* Shows a right arrow in a circle besides the "Portfolio" */}
+                <span className="px-2 duration-300 group-hover:rotate-90">
+                  <BiRightArrowCircle size={30} />
+                </span>
+            </button>
           </div>
         </div>
 
+        {/* // TODO: CHANGE INTO A PROFESSIONAL PICTURE OR JUST SIMPLY A PICTURE OF MYSELF
+        */}
+        {/* Profile picture of the creator */}
         <div>
-          <img src={heroImg} alt="hero_image"
+          <img src={heroImg} alt="profile_picture"
           className="w-2/3 mx-auto bg-gradient-to-r from-cyan-500 to-blue-500 rounded-2xl md:w-full" />
         </div>
       </div>      
