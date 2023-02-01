ff185ce6aa3fa8f86a03e992e5028b0fb263b772
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-01 22:48:34 +0800

    Imported all assets
    created index.jsx in assets folder for easier importing and using in components

---
+++ src/assets/index.jsx
@@ -0,0 +1,26 @@
+import css from './css.png'
+import github from './github.png'
+import graphql from './graphql.png'
+import heroImg from './heroImg.png'
+import html from './html.png'
+import icon from './icon.jpg'
+import javascript from './javascript.png'
+import nextjs from './nextjs.png'
+import node from './node.png'
+import react from './react.png'
+import tailwind from './tailwind.png'
+
+export
+{
+    css,
+    github,
+    graphql,
+    heroImg,
+    html,
+    icon,
+    javascript,
+    nextjs,
+    node,
+    react,
+    tailwind
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+// imports the heroImg from assets
+import { heroImg } from '../assets'
 
 const Hero = () => {
   return (
