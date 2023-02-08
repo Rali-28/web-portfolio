1b4d99c80571313dfa018f37b654c7d69441b12f
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-08 23:30:35 +0800

    Create index.jsx
    Created an index.jsx files for the portfolio folder that exports all jpg files for quick importing.

---
+++ src/assets/portfolio/index.jsx
@@ -0,0 +1,18 @@
+import { arrayDestruct } from './arrayDestruct.jpg'
+import { installNode } from './installNode.jpg'
+import { navbar } from './navbar.jpg'
+import { reactParallax } from './reactParallax.jpg'
+import { reactSmooth } from './reactSmooth.jpg'
+import { reactWeather } from './reactWeather.jpg'
+import { usestate } from './usestate.jpg'
+
+export
+{
+    arrayDestruct,
+    installNode,
+    navbar,
+    reactParallax,
+    reactSmooth,
+    reactWeather,
+    usestate
+}
