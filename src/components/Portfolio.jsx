0470f99c0bdb09846ed447d297eec97d978cbc96
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-08 23:33:51 +0800

    JPG imports
    Imported jpg for a preview of portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+// * Imports pictures.jpgs from asset/portfolio for showing a preview of the project
+import { arrayDestruct, installNode, navbar, reactParallax, reactSmooth, reactWeather } from '../assets/portfolio'
 
 const Portfolio = () => {
   return (
