38cd9e5de43b6d81a54a58eb4a376c624a44ed53
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-09 20:37:36 +0800

    Rendered contents for portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 // * Imports images for portfolio from constants/index.jsx
+import { portfolioImg } from '../constants'
 
 const Portfolio = () => {
   return (
@@ -11,13 +12,19 @@ const Portfolio = () => {
       </div>
 
       <div>
-        <div>
-          <img src="" alt="" />
-          <div>
-            <button>Demo</button>
-            <button>Code</button>
+        {portfolioImg.map(({id, title, img}) => (
+          <div
+            key={id}
+          >
+            <img src={img} alt={title} />
+            
+            <div>
+              <button>Demo</button>
+              <button>Code</button>
+            </div>
           </div>
-        </div>
+        ))}
+        
       </div>
     </div>
   )
