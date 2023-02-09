d7003065355f5d286d26a431bc707109d2cf3e30
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-09 20:25:37 +0800

    Update index.jsx

--- src/assets/portfolio/index.jsx
+++ src/assets/portfolio/index.jsx
@@ -1,10 +1,10 @@
-import { arrayDestruct } from './arrayDestruct.jpg'
-import { installNode } from './installNode.jpg'
-import { navbar } from './navbar.jpg'
-import { reactParallax } from './reactParallax.jpg'
-import { reactSmooth } from './reactSmooth.jpg'
-import { reactWeather } from './reactWeather.jpg'
-import { usestate } from './usestate.jpg'
+import arrayDestruct  from './arrayDestruct.jpg'
+import installNode  from './installNode.jpg'
+import navbar from './navbar.jpg'
+import reactParallax from './reactParallax.jpg'
+import reactSmooth from './reactSmooth.jpg'
+import reactWeather from './reactWeather.jpg'
+import usestate from './usestate.jpg'
 
 export
 {
