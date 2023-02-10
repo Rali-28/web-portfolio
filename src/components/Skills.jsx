efaff510dd94abb7813226fe8be624de33d8c51b
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-10 15:01:39 +0800

    Added styling to content
    Proper styling for rendering contents of skills section.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,21 +5,25 @@ import { skillsImg } from '../constants'
 const Skills = () => {
   return (
     // * Main container div
-    <div id='skills' name='skills'>
+    <div id='skills' name='skills' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black'>
 
-      <div>
+      <div className='flex flex-col justify-center max-w-screen-lg p-4 mx-auto text-white'>
         {/* Header container div */}
         <div>
-          <p>Experice</p>
-          <p>Technologies that I use and practice.</p>
+          <p className='inline p-2 text-4xl font-bold border-b-4 border-gray-500'>Experience</p>
+          <p className='py-6'>Technologies that I use and practice.</p>
         </div>
         {/* End header */}
 
-        {/* Skills images */}
-        <div>
+        {/* 
+          // * Skills images
+          // * Renders in 3 columns for big screens
+          // * Renders in 2 or < columns for smaller screens  
+        */}
+        <div className='grid w-full gap-8 px-12 py-8 text-center sm:grid-cols-2 md:grid-cols-3 sm:px-0'>
           {skillsImg.map(({id, title, img}) => (
-            <div key={id}>
-              <img src={img} alt={title} />
+            <div key={id} className='py-2 duration-500 rounded-lg shadow-md hover:scale-105'>
+              <img src={img} alt={title} className='w-20 mx-auto' />
               <p>{title}</p>
             </div>
           ))}
