d2cb0d48a9b55af9d3d1ae5a6c9902aa2a8b123c
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-11 21:08:55 +0800

    Rendered content for contact
    Added the contents for the contact section with minimal styling.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,66 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+
+    // * Main container div for the contact section
+    <div>
+
+      {/* 
+        // * Container div for arranging and styling the contact section
+      */}
+      <div>
+
+        {/* 
+          // * Header container
+        */}
+        <div>
+          <p>Contact</p>
+          <p>Send me a message to get in touch.</p>
+        </div>
+
+        {/* 
+          // * Forms container
+          // * Forms that will enable the viewer to send me a message
+        */}
+        <div>
+          <form action=''>
+
+            {/* Name of Sender Input */}
+            <input 
+              type='text'
+              name='name'
+              placeholder='Enter your name'
+              className='contact__form-input'
+            />
+
+            {/* Email address of sender Input */}
+            <input 
+              type='text'
+              name='email'
+              placeholder='Enter your email'
+              className='contact__form-input'
+            />
+
+            {/* Message Subject Input */}
+            <input 
+              type='text'
+              name='subject'
+              placeholder='Subject'
+              className='contact__form-input'
+            />
+            {/* Message input */}
+            <textarea 
+              name="message" 
+              rows="10"
+              className='contact__form-input'
+            >
+              
+            </textarea>
+          </form>
+        </div>
+      </div>
+      {/* // * End container div */}
+    </div> // * End main div
   )
 }
 
