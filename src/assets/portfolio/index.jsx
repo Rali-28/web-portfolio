b159904b44052585695974acf9583250a3acde3b
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-04-02 10:35:02 +0800

    Remove portfolio image placeholder

--- src/assets/portfolio/index.jsx
+++ src/assets/portfolio/index.jsx
@@ -1,18 +1,6 @@
-import arrayDestruct  from './arrayDestruct.jpg'
-import installNode  from './installNode.jpg'
-import navbar from './navbar.jpg'
-import reactParallax from './reactParallax.jpg'
-import reactSmooth from './reactSmooth.jpg'
-import reactWeather from './reactWeather.jpg'
-import usestate from './usestate.jpg'
+
 
 export
 {
-    arrayDestruct,
-    installNode,
-    navbar,
-    reactParallax,
-    reactSmooth,
-    reactWeather,
-    usestate
+ 
 }
